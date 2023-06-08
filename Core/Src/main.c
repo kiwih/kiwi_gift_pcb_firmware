a042e29fc6bda4ec4c8ce11866f28a50e2f6178a
kiwih <hammondpearce@outlook.com>
2023-06-08 11:08:00 -0400

    Program one and two

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,12 +19,12 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "usb_device.h"
-#include "usbd_cdc_if.h"
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+
 /* Private includes ----------------------------------------------------------*/
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "usbd_cdc_if.h"
 /* USER CODE BEGIN Includes */
 
 /* USER CODE END Includes */
@@ -59,7 +59,7 @@ static void MX_GPIO_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-#define LED_BRIGHTNESS_CYCLES 30
+#define LED_BRIGHTNESS_CYCLES 40
 
 //an array of structs where the struct contains indexes in the array and a length
 typedef struct {
@@ -174,7 +174,7 @@ uint8_t leds_get_random_index() {
 		if(index != 9) {
 			return index;
 		}
-	} while(true);
+	} while(1);
 }
 
 
@@ -236,7 +236,7 @@ uint8_t leds_program_one_step() {
 			//if we are past the midpoint (velocity is positive and brightness is at least 10,
 			// or velocity is negative and brightness is at most 10), propagate the velocity to linked leds
 			if((leds_status[i].velocity > 0 && leds_status[i].brightness >= LED_BRIGHTNESS_CYCLES/2) ||
-			   (leds_status[i].velocity < 0 && leds_status[i].brightness <= LED_BRIGHTNESS_CYCLES/2)) {
+			   (leds_status[i].velocity < 0 && leds_status[i].brightness <= LED_BRIGHTNESS_CYCLES - (LED_BRIGHTNESS_CYCLES/2))) {
 				for(uint8_t j = 0; j < led_links[i].len; j++) {
 					if(leds_status[i].velocity > 0 && leds_status[led_links[i].indexes[j]].brightness == 0) {
 						leds_status[led_links[i].indexes[j]].velocity = leds_status[i].velocity;
@@ -266,7 +266,7 @@ int8_t leds_program_two_single_led_step(uint8_t i) {
 	return leds_program_one_single_led_step(i);
 }
 
-uint8_t leds_program_two_fill_step() {
+uint8_t leds_program_two_step() {
 	//for each led which has a brightness of at least 1, increment its brightness by 1 to a maximum of 20
 	//then, for each led with brightness at least 10, set the brightness of its linked leds to 1 if they are not already at least 1
 	//return the number of LEDs that changed in this cycle
@@ -280,6 +280,142 @@ uint8_t leds_program_two_fill_step() {
 		}
 	}
 	return n_changed;
+}
+
+#define BASE_AWAIT_CYCLE_COUNT (LED_BRIGHTNESS_CYCLES * 1)
+
+enum program_state_e {
+	program_one_fill_init,
+	program_one_fill,
+	program_one_fill_eye,
+	program_one_unfill_init,
+	program_one_unfill,
+	program_one_unfill_eye,
+
+	program_two_fill_init,
+	program_two_fill,
+	program_two_fill_eye,
+	program_two_unfill_init,
+	program_two_unfill_eye,
+	program_two_unfill,
+
+	program_three_init,
+	program_three
+};
+
+void advance_state(int* program_repeat_count, enum program_state_e *program_state, uint32_t cycle_count, uint32_t *await_cycle_count) {
+	switch(*program_state) {
+	case program_one_fill_init:
+		leds_program_one_fill_init(leds_get_random_index(), 0, 1);
+		*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 6;
+		*program_state = program_one_fill;
+		break;
+	case program_one_fill:
+		if(leds_program_one_step() == 0) {
+			leds_program_one_eye_init(0, 1);
+			*program_state = program_one_fill_eye;
+			*await_cycle_count = 0;
+		}
+		break;
+	case program_one_fill_eye:
+		*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 10;
+		if(leds_program_one_single_led_step(9) == 0) {
+			*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 100;
+			if(cycle_count == BASE_AWAIT_CYCLE_COUNT * 100) {
+				*program_state = program_one_unfill_init;
+				*await_cycle_count = 0;
+			}
+		}
+		break;
+	case program_one_unfill_init:
+			leds_program_one_fill_init(leds_get_random_index(), LED_BRIGHTNESS_CYCLES, -1);
+			*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 6;
+			*program_state = program_one_unfill;
+			break;
+	case program_one_unfill:
+		if(leds_program_one_step() == 0) {
+			leds_program_one_eye_init(LED_BRIGHTNESS_CYCLES, -1);
+			*program_state = program_one_unfill_eye;
+			*await_cycle_count = 0;
+		}
+		break;
+	case program_one_unfill_eye:
+		*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 10;
+		if(leds_program_one_single_led_step(9) == 0) {
+			*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 100;
+			if(cycle_count == BASE_AWAIT_CYCLE_COUNT * 100) {
+				if(*program_repeat_count < 3) {
+					*program_state = program_one_fill_init;
+					(*program_repeat_count)++;
+				} else {
+					*program_state = program_two_fill_init;
+					(*program_repeat_count)=0;
+				}
+				*await_cycle_count = 0;
+			}
+		}
+		break;
+
+	case program_two_fill_init:
+		leds_program_two_border_init(0, 1);
+		leds_program_two_eye_init(0,0);
+		*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 6;
+		*program_state = program_two_fill;
+		break;
+	case program_two_fill:
+		if(leds_program_two_step() == 0) {
+			leds_program_two_eye_init(0, 1);
+			*program_state = program_two_fill_eye;
+			*await_cycle_count = 0;
+		}
+		break;
+	case program_two_fill_eye:
+		*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 10;
+		if(leds_program_one_single_led_step(9) == 0) {
+			*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 100;
+			if(cycle_count == BASE_AWAIT_CYCLE_COUNT * 100) {
+				*program_state = program_two_unfill_init;
+				*await_cycle_count = 0;
+			}
+		}
+		break;
+	case program_two_unfill_init:
+			leds_program_two_border_init(LED_BRIGHTNESS_CYCLES, -1);
+			leds_program_two_eye_init(LED_BRIGHTNESS_CYCLES, -1);
+			*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 6;
+			*program_state = program_two_unfill_eye;
+			break;
+	case program_two_unfill_eye:
+		*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 10;
+		if(leds_program_one_single_led_step(9) == 0) {
+			//*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 100;
+			//if(cycle_count == BASE_AWAIT_CYCLE_COUNT * 100) {
+				*program_state = program_two_unfill;
+			//}
+		}
+		break;
+	case program_two_unfill:
+		if(leds_program_two_step() == 0) {
+			*await_cycle_count = BASE_AWAIT_CYCLE_COUNT * 100;
+			if(cycle_count == BASE_AWAIT_CYCLE_COUNT * 100) {
+				if(*program_repeat_count < 1) {
+					*program_state = program_two_fill_init;
+					(*program_repeat_count)++;
+				} else {
+					*program_state = program_three_init;
+					(*program_repeat_count)=0;
+				}
+				*await_cycle_count = 0;
+			}
+		}
+		break;
+
+	case program_three_init:
+
+
+	default:
+		*program_state = program_one_fill_init;
+	}
 }
 
 /////////
@@ -427,223 +563,192 @@ void RxText(uint8_t* Buf, uint32_t *Len) {
 	mylen += *Len;
 }
 
-enum program_state_e {
-	program_one_init,
-	program_one_fill,
-	program_one_fill_eye,
-	program_one_unfill,
-	program_one_unfill_eye
-};
 
-void advance_state(enum program_state_e *program_state, uint32_t cycle_count, uint32_t *await_cycle_count) {
-	static uint8_t fill = 0;
-	static int8_t velocity = 1;
-	static int stabletcount = 0;
-	if(leds_program_one_step() == 0) {
-		if(stabletcount == 0) {
-			leds_program_one_eye_init(fill, velocity);
-		}
-		stabletcount++;
-		//leds_status[9].velocity = velocity;
-		if(leds_program_one_single_led_step(9) == 0 && stabletcount > 100) {
-			if(stabletcount > 10) {
-				stabletcount = 0;
-
-				if(velocity == 1) {
-					velocity = -1;
-					fill = LED_BRIGHTNESS_CYCLES;
-				}
-				else {
-					velocity = 1;
-					fill = 0;
-				}
-				uint8_t startindex = leds_get_random_index();
-				leds_program_one_fill_init(startindex, fill, velocity);
-
-			}
-		}
-	}
-}
 
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
-	/* USER CODE BEGIN 1 */
+  /* USER CODE BEGIN 1 */
 
-	/* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 	__HAL_AFIO_REMAP_SWJ_DISABLE(); //necessary as LEDs are using JTAG pins, comment out if you want debug support
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_USB_DEVICE_Init();
-	//leds_set_brightness_all(0);
-	/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USB_DEVICE_Init();
+  /* USER CODE BEGIN 2 */
 
-	/* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
-	enum program_state_e program_state = program_one_init;
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+    int program_repeat_count = 0;
+	enum program_state_e program_state = program_one_fill_init;
+	uint8_t led_cycle_count = 0;
 	uint32_t cycle_count = 0;
 	uint32_t await_cycle_count = 480;
 
-	int8_t velocity = 1;
-	uint8_t fill = 0;
-	uint8_t startindex = leds_get_random_index();
-	leds_program_one_fill_init(startindex, fill, velocity);
+//	int8_t velocity = 1;
+//	uint8_t fill = 0;
+//	uint8_t startindex = leds_get_random_index();
+//	leds_program_one_fill_init(startindex, fill, velocity);
 	//make a random number between 0 and 32 for startindex
 	while (1)
 	{
-		/* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-		/* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
 		ProcessText();
 		if(cycle_count >= await_cycle_count) {
-			advance_state(&program_state, cycle_count, &await_cycle_count);
+			advance_state(&program_repeat_count, &program_state, cycle_count, &await_cycle_count);
+			//leds_status[1].brightness = 1;
 			cycle_count = 0;
 		}
-
 		cycle_count++;
 
-		leds_emit((cycle_count % LED_BRIGHTNESS_CYCLES) + 1);
+		led_cycle_count++;
+		if(led_cycle_count > LED_BRIGHTNESS_CYCLES)
+			led_cycle_count = 1;
+
+		leds_emit(led_cycle_count);
 
 
 		//CDC_Transmit_FS("hi\n", 3);
 		//HAL_Delay(1);
 	}
-	/* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
-	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL3;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL3;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-	{
-		Error_Handler();
-	}
-	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
-	PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
-	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_GPIO_Init(void)
 {
-	GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-	/* GPIO Ports Clock Enable */
-	__HAL_RCC_GPIOC_CLK_ENABLE();
-	__HAL_RCC_GPIOD_CLK_ENABLE();
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	__HAL_RCC_GPIOB_CLK_ENABLE();
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOC, LED3_Pin|LED2_Pin|LED1_Pin, GPIO_PIN_RESET);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, LED3_Pin|LED2_Pin|LED1_Pin, GPIO_PIN_RESET);
 
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOA, LED33_Pin|LED32_Pin|LED31_Pin|LED30_Pin
-			|LED29_Pin|LED28_Pin|LED27_Pin|LED26_Pin
-			|LED16_Pin|LED15_Pin|LED14_Pin|LED13_Pin
-			|LED12_Pin|LED11_Pin, GPIO_PIN_RESET);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED33_Pin|LED32_Pin|LED31_Pin|LED30_Pin
+                          |LED29_Pin|LED28_Pin|LED27_Pin|LED26_Pin
+                          |LED16_Pin|LED15_Pin|LED14_Pin|LED13_Pin
+                          |LED12_Pin|LED11_Pin, GPIO_PIN_RESET);
 
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOB, LED25_Pin|LED24_Pin|LED23_Pin|LED22_Pin
-			|LED21_Pin|LED20_Pin|LED19_Pin|LED18_Pin
-			|LED17_Pin|LED10_Pin|LED9_Pin|LED8_Pin
-			|LED7_Pin|LED6_Pin|LED5_Pin|LED4_Pin, GPIO_PIN_RESET);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LED25_Pin|LED24_Pin|LED23_Pin|LED22_Pin
+                          |LED21_Pin|LED20_Pin|LED19_Pin|LED18_Pin
+                          |LED17_Pin|LED10_Pin|LED9_Pin|LED8_Pin
+                          |LED7_Pin|LED6_Pin|LED5_Pin|LED4_Pin, GPIO_PIN_RESET);
 
-	/*Configure GPIO pins : LED3_Pin LED2_Pin LED1_Pin */
-	GPIO_InitStruct.Pin = LED3_Pin|LED2_Pin|LED1_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  /*Configure GPIO pins : LED3_Pin LED2_Pin LED1_Pin */
+  GPIO_InitStruct.Pin = LED3_Pin|LED2_Pin|LED1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-	/*Configure GPIO pins : LED33_Pin LED32_Pin LED31_Pin LED30_Pin
-						   LED29_Pin LED28_Pin LED27_Pin LED26_Pin
-						   LED16_Pin LED15_Pin LED14_Pin LED13_Pin
-						   LED12_Pin LED11_Pin */
-	GPIO_InitStruct.Pin = LED33_Pin|LED32_Pin|LED31_Pin|LED30_Pin
-			|LED29_Pin|LED28_Pin|LED27_Pin|LED26_Pin
-			|LED16_Pin|LED15_Pin|LED14_Pin|LED13_Pin
-			|LED12_Pin|LED11_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  /*Configure GPIO pins : LED33_Pin LED32_Pin LED31_Pin LED30_Pin
+                           LED29_Pin LED28_Pin LED27_Pin LED26_Pin
+                           LED16_Pin LED15_Pin LED14_Pin LED13_Pin
+                           LED12_Pin LED11_Pin */
+  GPIO_InitStruct.Pin = LED33_Pin|LED32_Pin|LED31_Pin|LED30_Pin
+                          |LED29_Pin|LED28_Pin|LED27_Pin|LED26_Pin
+                          |LED16_Pin|LED15_Pin|LED14_Pin|LED13_Pin
+                          |LED12_Pin|LED11_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-	/*Configure GPIO pins : LED25_Pin LED24_Pin LED23_Pin LED22_Pin
-						   LED21_Pin LED20_Pin LED19_Pin LED18_Pin
-						   LED17_Pin LED10_Pin LED9_Pin LED8_Pin
-						   LED7_Pin LED6_Pin LED5_Pin LED4_Pin */
-	GPIO_InitStruct.Pin = LED25_Pin|LED24_Pin|LED23_Pin|LED22_Pin
-			|LED21_Pin|LED20_Pin|LED19_Pin|LED18_Pin
-			|LED17_Pin|LED10_Pin|LED9_Pin|LED8_Pin
-			|LED7_Pin|LED6_Pin|LED5_Pin|LED4_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  /*Configure GPIO pins : LED25_Pin LED24_Pin LED23_Pin LED22_Pin
+                           LED21_Pin LED20_Pin LED19_Pin LED18_Pin
+                           LED17_Pin LED10_Pin LED9_Pin LED8_Pin
+                           LED7_Pin LED6_Pin LED5_Pin LED4_Pin */
+  GPIO_InitStruct.Pin = LED25_Pin|LED24_Pin|LED23_Pin|LED22_Pin
+                          |LED21_Pin|LED20_Pin|LED19_Pin|LED18_Pin
+                          |LED17_Pin|LED10_Pin|LED9_Pin|LED8_Pin
+                          |LED7_Pin|LED6_Pin|LED5_Pin|LED4_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
@@ -652,33 +757,33 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
-	/* USER CODE BEGIN Error_Handler_Debug */
+  /* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1)
 	{
 	}
-	/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-	/* USER CODE BEGIN 6 */
+  /* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
 	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-	/* USER CODE END 6 */
+  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
